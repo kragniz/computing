465d5f601e70f117409da4449334aade2c7ac065
Louis Taylor <kragniz@gmail.com>
2011-11-01 12:06:27 -0400

    finished fibonacci, added caching

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 import sys
 
-class cache(object):
-    def __main__(self):
+class fibonacci(object):
+    def __init__(self):
+        self.numbers = {}
 
+    def getNth(self, n):
+        if self.numbers.has_key(n):
+            return self.numbers[n]
+        else:
+            return False
 
-def f(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    else:
-        return f(n - 1) + f(n - 2)
+    def setNth(self, n, v):
+        self.numbers[n] = v
+
+    def fib(self, n):
+        v = self.getNth(n)
+        if v:
+            return v
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        else:
+            nthFib = self.fib(n - 1) + self.fib(n - 2)
+            self.setNth(n, nthFib)
+            return nthFib
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -19,5 +34,6 @@ if __name__ == '__main__':
     else:
         n = 10
 
+    f = fibonacci()
     for i in range(n):
-        print f(i),
+        print f.fib(i),
