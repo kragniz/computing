065554978e7f2404620db6537a9d94c12cf34e62
Louis Taylor <kragniz@gmail.com>
2012-03-13 15:54:24 +0000

    Added some error handling

--- collatz/stopping.py
+++ collatz/stopping.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-def collatz(n):
-    x = n
+def collatz(x):
     i = 0
     while x != 1:
         if not x % 2:
@@ -12,15 +11,17 @@ def collatz(n):
         i += 1
     return i
 
-
 if __name__ == '__main__':
-    i = 1
+    i = int(sys.argv[1])
     high = 0
     highesti = 0
-    while True:
-        n = collatz(i)
-        if n > high:
-            high = n
-            highesti = i
-            print high, highesti
-        i += 1
+    try:
+        while True:
+            n = collatz(i)
+            if n > high:
+                high = n
+                highesti = i
+                print high, highesti
+            i += 1
+    except:
+        print '\nyou got to i =', i
